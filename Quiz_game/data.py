e486538e98a32a37896ede4b6e10e8edb0da0c7a
Dev Shah <devshah1682003@gmail.com>
2025-09-09 23:04:37 +0530

    Added Quiz Game

---
+++ Quiz_game/data.py
@@ -0,0 +1,13 @@
+question_data=[
+{"text":"A slug's blood is green","answer":"True"},
+{"text":"The Great Wall of China is visible from space","answer":"False"},
+{"text":"Octopuses have three hearts","answer":"True"},
+{"text":"Humans and dinosaurs lived at the same time","answer":"False"},
+{"text":"Bananas are berries","answer":"True"},
+{"text":"The capital of Australia is Sydney","answer":"False"},
+{"text":"Lightning can strike the same place twice","answer":"True"},
+{"text":"Bats are blind","answer":"False"},
+{"text":"Venus is the hottest planet in our solar system","answer":"True"},
+{"text":"Penguins can fly","answer":"False"},
+{"text":"The human body has four lungs","answer":"False"}
+]
